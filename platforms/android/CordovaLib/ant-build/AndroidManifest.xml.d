c740bad3260041c4c15dbb61d6ee5012963fcc8b
unknown <Wilco@WILCO-PC.prima.local>
2014-08-26 15:58:44 +0200

    Full

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\android_prac\camera_app\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\android_prac\camera_app\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\android_prac\camera_app\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
