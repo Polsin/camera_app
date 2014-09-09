7b20ec972848d8aa67441cbac58412d1104c975d
ssslow <ssslow@gmail.com>
2014-09-09 10:34:48 +0200

    Front camera for real!

---
+++ plugins/com.phonegap.plugins.barcodescanner/src/android/LibraryProject/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Johan\Documents\apps\camera_app\plugins\com.phonegap.plugins.barcodescanner\src\android\LibraryProject\bin\AndroidManifest.xml : \
+C:\Users\Johan\Documents\apps\camera_app\plugins\com.phonegap.plugins.barcodescanner\src\android\LibraryProject\AndroidManifest.xml \
